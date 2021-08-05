54566e5bedbedb286154bb3657d33801040f7b90
Simon Kemmerling <simon.kemmerling@brandung.de>
2021-08-05 19:45:38 +0200

    add filter function

--- src/components/Entries/Entries.js
+++ src/components/Entries/Entries.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
 
 import EntryItem from "./EntryItem/EntryItem";
 import TotalTime from "./TotalTime/TotalTime";
+import EntriesFilter from "./EntriesFilter/EntriesFilter";
+
+const filterDefault = {
+  type: "all",
+  value: "",
+};
 
 const Entries = ({ entries }) => {
+  const [selectedFilter, setSelectedFilter] = useState(filterDefault);
+
+  const filterChangeHandler = (filter) => {
+    setSelectedFilter(filter);
+  };
+
+  const filteredEntries = entries.filter((entry) => {
+    const entryDate = moment(entry.date);
+
+    const currentDate = moment();
+    const FROM_DATE = currentDate.clone().startOf("isoWeek");
+    const TO_DATE = currentDate.clone().endOf("isoWeek");
+
+    switch (selectedFilter.type) {
+      case "today":
+        return entryDate.format("DD") === currentDate.format("DD");
+      case "week":
+        return entryDate.isBetween(FROM_DATE, TO_DATE);
+      case "month":
+        return entryDate.format("MM") === currentDate.format("MM");
+      case "individual":
+        return entryDate.format("DD") === moment(selectedFilter.value).format("DD");
+      default:
+        return entry;
+    }
+  });
+
   return (
     <>
-      <TotalTime entries={entries} />
-      {entries.map((entry) => (
+      <EntriesFilter
+        selected={selectedFilter}
+        onChangeFilter={filterChangeHandler}
+      />
+      <TotalTime entries={filteredEntries} />
+      {filteredEntries.map((entry) => (
         <EntryItem key={entry.id} entryData={entry} />
       ))}
     </>
---
+++ src/components/Entries/EntriesFilter/EntriesFilter.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import DatePicker from "../../Ui/DatePicker";
+
+const rangeFilter = [
+  { id: 1, value: "all", filterName: "Alle" },
+  { id: 2, value: "today", filterName: "Heute" },
+  { id: 3, value: "week", filterName: "Woche" },
+  { id: 4, value: "month", filterName: "Monat" },
+  { id: 5, value: "individual", filterName: "Datum wählen..." },
+];
+
+const EntriesFilter = ({ selected, onChangeFilter }) => {
+  const [filter, setFilter] = useState(selected);
+  const [dateSelected, setDateSelected] = useState(false);
+
+  const dropdownChangeHandler = (event) => {
+    setDateSelected(false);
+    const typeName = event.target.value;
+    let dateStr = "";
+
+    if (typeName === "individual") {
+      setDateSelected(true);
+      dateStr = new Date().toISOString();
+    }
+
+    setFilter((prefs) => {
+      return {
+        ...prefs,
+        type: typeName,
+        value: dateStr,
+      };
+    });
+  };
+
+  useEffect(() => {
+    onChangeFilter(filter);
+  }, [filter]);
+
+  const individualChangeHandler = (updatedDate) => {
+    setFilter({
+      type: "individual",
+      value: updatedDate,
+    });
+  };
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6}>
+        <FormControl fullWidth>
+          <InputLabel shrink>Zeitraum</InputLabel>
+          <Select
+            name="range"
+            value={filter.type}
+            onChange={dropdownChangeHandler}
+          >
+            {rangeFilter.map((item) => (
+              <MenuItem key={item.id} value={item.value}>
+                {item.filterName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      {dateSelected && (
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <DatePicker
+              name="date"
+              onUpdateDate={individualChangeHandler}
+              label="Datum auswählen"
+            />
+          </FormControl>
+        </Grid>
+      )}
+    </Grid>
+  );
+};
+
+export default EntriesFilter;

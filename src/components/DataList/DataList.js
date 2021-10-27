16b8738bf0e3e1e7faa4f226650f4b5c167ffe96
Simon Kemmerling <simon.kemmerling@brandung.de>
2021-10-27 11:58:24 +0200

    add options for datagrid

--- src/components/DataList/DataList.js
+++ src/components/DataList/DataList.js
@@ -50,8 +50,10 @@ const DataList = () => {
           rows={entries}
           columns={columns}
           pageSize={5}
+          rowsPerPageOptions={[5, 10, 20]}
           checkboxSelection
           disableSelectionOnClick
+          pagination
         />
       </div>
     );
